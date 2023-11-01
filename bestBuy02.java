229708577576da4b5b9a2cb8d49271c4feae0940
sampetersen22 <148434191+sampetersen22@users.noreply.github.com>
2023-11-01 13:25:43 +0530

    Update bestBuy02.java
    Edited

--- bestBuy02.java
+++ bestBuy02.java
@@ -8,7 +8,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class bestBuy02 {
 
-	public static void main(String[] args) throws InterruptedException {
+	
+        @Test
+	public void test03() throws InterruptedException {
 		// TODO Auto-generated method stub
 		
 		System.setProperty("webdriver.chrome.driver","D:\\Java -Aut Test\\WebDriver\\Drivers\\chromedriver.exe");
