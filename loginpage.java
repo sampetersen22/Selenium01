d6c7ad5f5c8e8c52d7e52155e6c072dd05ea4ee0
sampetersen22 <148434191+sampetersen22@users.noreply.github.com>
2023-11-30 15:47:54 +0530

    Update loginpage.java

--- loginpage.java
+++ loginpage.java
@@ -26,57 +26,77 @@ public class loginpage extends pageMethods {
 	
 	@FindBy(xpath="//body//div[@id='react-root']//div[@data-testid='application-id']//div//div//div[5]//div[3]")
 	WebElement cred;
+	
 	@FindBy(xpath="//body//div[@id='react-root']//div[@data-testid='application-id']//div//div//div[5]//div[2]//div[1]//div[2]//div[2]")
 	WebElement cred1;
+	
 	@FindBy(xpath="//div[contains(text(),'USD')]")
 	WebElement cred2;
+	
 	@FindBy(xpath="(//div)[291]")
 	WebElement cred3;
+	
 	@FindBy(xpath="(//div[@data-focusable='true'])[20]")
 	WebElement cred4;
+	
 	@FindBy(xpath="//div[contains(text(),'From')]")
 	WebElement cred5;
+	
 	@FindBy(xpath="//div[normalize-space()='Kempegowda International Airport']")
 	WebElement cred6;
+	
 	@FindBy(xpath="//div[normalize-space()='Indira Gandhi International Airport']")
 	WebElement cred7;
+	
 	@FindBy(xpath="(//div)[483]")
 	WebElement cred8;
+	
 	@FindBy(xpath="(//div[@data-focusable='true'])[27]")
 	WebElement cred9;
+	
 	@FindBy(xpath="//body[1]/div[2]/div[1]/div[1]/div[1]/div[5]/div[1]/div[1]/div[3]/div[2]")
 	WebElement cred10;
+	
 	@FindBy(xpath="//input[@data-testid='traveller-0-first-traveller-info-input-box']")
 	WebElement cred11;
+	
 	@FindBy(xpath="//input[@data-testid='traveller-0-last-traveller-info-input-box']")
 	WebElement cred12;
+	
 	@FindBy(xpath="(//div[@data-focusable='true'])[13]")
 	WebElement cred13;
+	
 	@FindBy(xpath="//input[@data-testid='traveller-1-first-traveller-info-input-box']")
 	WebElement cred14;
+	
 	@FindBy(xpath="//input[@data-testid='traveller-1-last-traveller-info-input-box']")
 	WebElement cred15;
+	
 	@FindBy(xpath="(//div[@data-focusable='true'])[15]")
 	WebElement cred16;
+	
 	@FindBy(xpath="//div[@datla-testid='blueRibbonBag']//div[@data-testid='expandableList']//div[@aria-disabled='true']//div//div[@data-testid='expandableList-header']//div//div[@data-focusable='true']")
 	WebElement cred17;
+	
 	@FindBy(xpath="(//div[@dir='auto'][normalize-space()='BLR - DEL'])[1]")
 	WebElement cred18;
+	
 	@FindBy(xpath="(//div[contains(text(),'Done')])[1]")
 	WebElement cred19;
+	
 	@FindBy(xpath="//*[@id=\'main-container\']/div/div[5]/div/div/div[2]/div/div/div[4]")
 	WebElement cred20;
 	
 	@Test
 	public void logindetails() throws InterruptedException {
-		PageFactory.initElements(driver, this);
+        PageFactory.initElements(driver, this);
 		//WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(10));
 		//WebElement element = wait.until(ExpectedConditions.elementToBeClickable(loginBtn));
         loginBtn.click();
-	   Thread.sleep(5000);
-	   number.sendKeys("8667500480");
-	   pswd.sendKeys("Maxval123#");
-	   try {
+	Thread.sleep(5000);
+	number.sendKeys("8667500480");
+	pswd.sendKeys("Maxval123#");
+	try {
 		cred.click();
 		 test.log(Status.PASS, "Login successfully");
 	} catch (Exception e) {
@@ -87,7 +107,7 @@ public class loginpage extends pageMethods {
 	  
 	   cred1.click();
 	   cred2.click();
-	   cred3.click();
+           cred3.click();
 	   test.log(Status.PASS, "Currency selected");
 	   cred4.click();
 	   cred5.click();
@@ -107,12 +127,11 @@ public class loginpage extends pageMethods {
 	   test.log(Status.PASS, "Passenger 2 details given");
 	   cred16.click(); Thread.sleep(5000);
 	   test.log(Status.PASS, "Cost of tickets");
-	    cred17.click();
-		cred18.click();
-		cred19.click();
-		cred20.click();
+	   cred17.click();
+	   cred18.click();
+	   cred19.click();
+	   cred20.click();
 	   
-		//searchBtn.click();
 	}
 
 }
